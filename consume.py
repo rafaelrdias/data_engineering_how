3557366da02e8b27707a296cad496a8a21621285
Rafael Dias <43451646+rafaelrdias@users.noreply.github.com>
2022-03-07 19:36:39 -0300

    Scripts do módulo 18

---
+++ consume.py
@@ -0,0 +1,7 @@
+import boto3
+
+client = boto3.client('sqs')
+response = client.receive_message(
+    QueueUrl='https://sqs.us-east-1.amazonaws.com/695099411134/api-pagamentos-consumidor-criado')
+print(response)
+
